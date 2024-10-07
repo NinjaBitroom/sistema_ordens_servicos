61b98568faab31f59ba0435bfe01fbd03d38d0ed
Gabriel Bitencourt Marin <gabrielbitercourt@gmail.com>
2024-10-07 12:46:24 -0400

    feat: add SupplierIndexView

---
+++ src/views/supplier/supplier_index_view.py
@@ -0,0 +1,29 @@
+"""."""
+
+from collections.abc import Sequence
+from http import HTTPMethod
+
+from flask import render_template
+from flask.views import MethodView
+
+from src.models.fornecedor_model import FornecedorModel
+from src.protocols.controller import Controller
+from src.protocols.http.http_request import HttpRequest
+
+
+class SupplierIndexView(MethodView):
+    """."""
+
+    def __init__(
+        self,
+        controller: Controller[None, Sequence[FornecedorModel] | Exception],
+    ) -> None:
+        """."""
+        self.__CONTROLLER = controller
+
+    def get(self) -> object:
+        """."""
+        response = self.__CONTROLLER.handle(
+            HttpRequest(method=HTTPMethod.GET, body=None)
+        )
+        return render_template("supplier/index.html", clients=response.body)
