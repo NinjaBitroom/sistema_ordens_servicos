454e0a7e9457cc13530033df6c9fbbdebe81ac5c
Gabriel Bitencourt Marin <gabrielbitercourt@gmail.com>
2024-10-14 10:56:12 -0400

    feat: add SQLAlchemy-Utils

--- src/models/base/telefones_model.py
+++ src/models/base/telefones_model.py
@@ -1,6 +1,8 @@
 """."""
 
 from pydantic_extra_types.phone_numbers import PhoneNumber
+from sqlalchemy_utils import PhoneNumberType  # type: ignore  # noqa: PGH003
+from sqlmodel import Field  # type: ignore  # noqa: PGH003
 
 from src.services.extensions.database import DB
 
@@ -8,5 +10,5 @@ from src.services.extensions.database import DB
 class TelefonesModel(DB.Model):
     """."""
 
-    telefone_fixo: PhoneNumber | None
-    telefone_celular: PhoneNumber | None
+    telefone_fixo: PhoneNumber | None = Field(sa_type=PhoneNumberType)
+    telefone_celular: PhoneNumber | None = Field(sa_type=PhoneNumberType)
--- src/utils/sql_model_converter.py
+++ src/utils/sql_model_converter.py
@@ -1,6 +1,9 @@
 """."""
 
-from wtforms import StringField
+from collections.abc import Mapping
+from typing import Any
+
+from wtforms import StringField, TelField
 from wtforms_sqlalchemy.orm import (  # type: ignore  # noqa: PGH003
     ModelConverter,
     converts,  # type: ignore  # noqa: PGH003
@@ -15,4 +18,13 @@ class SqlModelConverter(ModelConverter):
         self, field_args: object, **extra: object
     ) -> StringField:
         """."""
+        StringField()
         return self.conv_String(field_args, **extra)  # type: ignore  # noqa: PGH003
+
+    @converts("sqlalchemy_utils.types.phone_number.PhoneNumberType")
+    def conv_phone_number(
+        self, field_args: Mapping[Any, Any], **extra: object
+    ) -> TelField:
+        """."""
+        self._string_common(field_args=field_args, **extra)  # type: ignore  # noqa: PGH003
+        return TelField(**field_args)
