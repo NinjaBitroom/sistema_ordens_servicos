baa12488664e00cab5cd9e2edeea18cf02e97b7b
Gabriel Bitencourt Marin <gabrielbitercourt@gmail.com>
2024-10-28 22:21:35 -0400

    feat: add service order item relationship

--- src/models/item_da_ordem_de_servico_model.py
+++ src/models/item_da_ordem_de_servico_model.py
@@ -7,6 +7,7 @@ from sqlmodel import (
 )
 
 from src.models.base.base_model import BaseModel
+from src.models.ordem_de_servico_model import OrdemDeServicoModel
 from src.models.produto_model import ProdutoModel
 
 
@@ -15,6 +16,12 @@ class ItemDaOrdemDeServicoModel(BaseModel, table=True):
 
     __tablename__ = "Itens da ordem de serviço"
     id: NonNegativeInt | None = Field(default=None, primary_key=True)
+    ordem_de_servico_id: NonNegativeInt | None = Field(
+        foreign_key="Ordens de serviço.id", nullable=False
+    )
+    ordem_de_servico: OrdemDeServicoModel | None = Relationship(
+        back_populates="itens_da_ordem_de_servico"
+    )
     produto_id: NonNegativeInt | None = Field(
         foreign_key="Produtos.id", nullable=False
     )
--- src/models/ordem_de_servico_model.py
+++ src/models/ordem_de_servico_model.py
@@ -1,6 +1,7 @@
 """."""
 
 from datetime import date
+from typing import TYPE_CHECKING
 
 from pydantic import NonNegativeFloat, NonNegativeInt
 from sqlalchemy import Text
@@ -13,12 +14,20 @@ from src.models.base.base_model import BaseModel
 from src.models.cliente_model import ClienteModel
 from src.models.funcionario_model import FuncionarioModel
 
+if TYPE_CHECKING:
+    from src.models.item_da_ordem_de_servico_model import (
+        ItemDaOrdemDeServicoModel,
+    )
+
 
 class OrdemDeServicoModel(BaseModel, table=True):
     """."""
 
     __tablename__ = "Ordens de serviço"
     id: NonNegativeInt | None = Field(default=None, primary_key=True)
+    itens_da_ordem_de_servico: list["ItemDaOrdemDeServicoModel"] = (
+        Relationship(back_populates="ordem_de_servico")
+    )
     tecnico_id: NonNegativeInt | None = Field(
         foreign_key="Funcionários.id", nullable=False
     )
@@ -30,6 +39,8 @@ class OrdemDeServicoModel(BaseModel, table=True):
     descricao_do_problema: str = Field(sa_type=Text)
     valor_total_da_ordem: NonNegativeFloat
     data_: date = Field(
-        default_factory=date.today, sa_column_kwargs={"name": "data"}
+        default_factory=date.today,
+        sa_column_kwargs={"name": "data"},
+        title="Data",
     )
     aberto: bool = Field(default=True)
